d73a3e714ec62eb128db6f5884ee72485c2ec89c
niteshjain007 <nitesh.iiitm@gmail.com>
2025-02-04 09:15:48 +0530

    env files are added

--- src/tests/LoginTest.spec.ts
+++ src/tests/LoginTest.spec.ts
@@ -2,7 +2,7 @@ import {test} from "@playwright/test";
 
 import LoginPage from "../pages/LoginPage";
 
-test("LoginWithValidCredential",async ({page})=>{
+test.skip("LoginWithValidCredential",async ({page})=>{
 
     const loginPageObj = new LoginPage(page);
 
@@ -13,4 +13,22 @@ test("LoginWithValidCredential",async ({page})=>{
    const homePageObj= await loginPageObj.clickOnLoginBtn();
    await homePageObj.clickOnPopupModalCloseButton();
     await homePageObj.verifyLogoIsPresent();
+});
+
+
+test("LoginWithEnvValidCredential",async ({page})=>{
+
+    const loginPageObj = new LoginPage(page);
+
+    await loginPageObj.gotoLoginPage();
+    await loginPageObj.fillUserName(process.env.userEmail!);
+    await loginPageObj.fillPassword(process.env.password!);
+
+   const homePageObj= await loginPageObj.clickOnLoginBtn();
+   await homePageObj.clickOnPopupModalCloseButton();
+    await homePageObj.verifyLogoIsPresent();
+
+   // console.log(process.env.NODE_ENV)
+  //  console.log(process.env.userEmail);
+   // console.log(process.env.password);
 });
